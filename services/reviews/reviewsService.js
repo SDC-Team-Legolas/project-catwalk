a904701bdedb3d1dc4d908d377442232ece8b8ac
Gabriel Wright <wrightgabriel0220@gmail.com>
2021-06-26 15:48:13 -0700

    Implemented /reviews endpoint w/ sorting and correct parameter behaviors

--- services/reviews/reviewsService.js
+++ services/reviews/reviewsService.js
@@ -3,6 +3,9 @@ const app = express();
 const { Client } = require('pg');
 const dbConfig = require('./config-env/config.js');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 let port = 1234;
 let client = new Client(`postgres://${dbConfig.user}:${dbConfig.pass}@project-catwalk_db_1:5432/${dbConfig.db}`);
 
@@ -17,6 +20,34 @@ client.connect((err, success) => {
 
 app.get('/', (req, res) => {
   res.send('Responding to GET request at /');
+});
+
+app.get('/reviews', (req, res) => {
+  let page = req.body.page || 1;
+  let count = req.body.count || 5;
+  let sort = { helpful: 'helpfulness', newest: 'date', relevant: 'rating'}[req.body.sort];
+  client.query(`SELECT * FROM reviews WHERE (product_id = ${req.body.product_id}) LIMIT ${count}`)
+    .then(response => {
+      let sortedReviews = response.rows.sort((rowA, rowB) => {
+        console.log('rowA: ', rowA[sort]);
+        console.log('rowB: ', rowB[sort]);
+        return rowB[sort] - rowA[sort];
+      });
+
+      let responseObj = {
+        product: req.body.product_id,
+        page: req.body.page,
+        count: req.body.count,
+        results: sortedReviews
+      };
+      console.log(responseObj);
+      res.send(responseObj);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500);
+      res.send(err);
+    });
 });
 
 app.listen(port, () => {
