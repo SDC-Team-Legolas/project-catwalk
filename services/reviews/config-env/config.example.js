923e184177b56da663c200e56f70949c6f498f47
Gabriel Wright <wrightgabriel0220@gmail.com>
2021-07-02 16:37:13 -0700

    Removed docker entirely (broken commit, but docker-free)

--- services/reviews/config-env/config.example.js
+++ services/reviews/config-env/config.example.js
@@ -1,5 +1,6 @@
 module.exports = {
-  pass: 'ENTER YOUR DB PASSWORD HERE',
+  password: 'ENTER YOUR DB PASSWORD HERE',
   user: 'ENTER YOUR DB USERNAME HERE',
+  host: 'ENTER YOUR DB IPv6/DNS ADDRESS HERE (db name if docker-composing both images on the same machine)',
   db: 'ENTER DB NAME HERE'
 };
